36c43037f7d3d4713b0a212cd26679ab4d1eb8f7
25ELIJAH <easterjames420@gmail.com>
2026-05-05 14:21:07 +0300

    fixed mainfest and service

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
-worker
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
